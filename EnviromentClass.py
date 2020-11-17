1f87346b9ce8416096ec90faab67266f6aafc479
Emil Johansson <emil@Emils-MacBook-Pro.local>
2020-11-17 18:27:38 +0100

    Switched enviroment to it's own file and done a 9x9 deterministic enviroment

---
+++ EnviromentClass.py
@@ -0,0 +1,32 @@
+import numpy as np
+import random
+#Enviroment Class. 
+class Enviroment:
+    def __init__(self,gridSize):
+        self.gridSize = gridSize
+        self.road = 0
+        self.building = 5
+        self.empty = 0
+        self.grid = np.zeros((gridSize,gridSize))
+    
+    def BuildEnviroment(self):
+        #TODO Check what is road and what is building to make a good approximation
+        for i in range(self.gridSize):
+            for j in range(self.gridSize):
+                r = random.random()
+                if r < 0.5:
+                    self.grid[i,j] = self.building
+                else:
+                    self.grid[i,j] = self.road
+
+
+    def DeterministicEnviroment(self):
+        #Fix different cells start with 9
+        v1 = [0,1,2,6,7,8]
+        v2 = [2,6]
+        self.grid[:,:] = self.road
+        for i in range(self.gridSize):
+            if i in v1:
+                self.grid[i,v2] = self.building
+            if i in v2:
+                self.grid[i,v1] = self.building
