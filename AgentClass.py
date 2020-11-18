26082d0dac78d335be165d0fc1e4a1171ab12331
Emil Johansson <emil@Emils-MacBook-Pro.local>
2020-11-18 11:48:35 +0100

    test

--- AgentClass.py
+++ AgentClass.py
@@ -3,6 +3,31 @@ import numpy as np
 
 class Agent:
     def __init__(self,numberAgents, grid):
-        self.grid = grid
-        
-        x = np.zeros()
+        self.grid = grid    
+        self.numberAgents = numberAgents
+        self.x = np.zeros(numberAgents) #Each agent has a x and y coordinate in the grid
+        self.y = np.zeros(numberAgents)
+        self.status = []
+
+    def agentRange(self, rangeLength):
+        self.status = []
+        for agent in range(self.numberAgents):
+            tmpX = self.x[agent]
+            tmpY = self.y[agent]
+            #Spread to neighbors but be careful of boundaries in grid "two step neighbor"
+            for reach in range(rangeLength):
+                #tmpX + 1,tmpY
+                #tmpX - 1,tmpY
+                #tmpX,tmpY + 1
+                #tmpX,tmpY + 1
+                if tmpX + 1 < np.size(self.grid[0])-1:
+                    tmp1 = (tmpX+1,tmpY)
+                if tmpX - 1 > 0
+                    tmp2 = (tmpX-1,tmpY)
+                if tmpY + 1 < np.size(self.grid[0])-1:
+                    tmp3 = (tmpX+1,tmpY)
+                if tmpY - 1 > 0
+                    tmp4 = (tmpX-1,tmpY)
+
+        self.status.append(tmpArray)
+
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import numpy as np
 import random
 import EnviromentClass
+import AgentClass
 
 def main():
     test = EnviromentClass.Enviroment(10) #Has to be 10 as of now.
     #test.BuildEnviroment()
     test.DeterministicEnviroment()
-    print(test.grid)
+    #print(test.grid)
+    agents = AgentClass.Agent(2,test.grid)
+    agents.agentRange(2)
+    print(agents.status)
+    
 
 
 
