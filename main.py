93a575d42776e9515a0daafdd5ec340406312b83
Emil Johansson <emil@Emils-MacBook-Pro.local>
2020-11-22 10:56:02 +0100

    AgentRange completed

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ def main():
     #print(test.grid)
     #print(test)
     agents = AgentClass.Agent(2,test.grid)
+    agents.x[1] = 2
+    agents.y[1] = 2
     agents.agentRange(2)
-    print(agents.status)
+    #print(agents.status)
+    print("A0\n",agents.status[0]) #Gets agent 0 status with x, y and some decay value...
+    print("A1\n",agents.status[1]) #Gets agent 0 status with x, y and some decay value...
+
     #I do a change
     
 
